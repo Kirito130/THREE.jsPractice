ec270419706080ca5a82411360864821dcfbcdd6
Kirito130 <burhanp1300@gmail.com>
2023-07-24 15:04:08 +0530

    Eight Commit

--- 3jsCrashCourse4/app.js
+++ 3jsCrashCourse4/app.js
@@ -123,6 +123,17 @@ scene.add(plane);
 plane.rotation.x = - 0.5 * Math.PI;
 plane.receiveShadow = true;
 
+// Plane2
+const plane2Geo = new THREE.PlaneGeometry(10, 10, 10, 10);
+const plane2Mat = new THREE.MeshStandardMaterial({
+    color: 0xFFFFFF,
+    wireframe: true,
+});
+const plane2 = new THREE.Mesh(plane2Geo, plane2Mat);
+scene.add(plane2);
+plane2.position.set(10, 10, 15);
+const lastPointZ = plane2.geometry.attributes.position.array.length - 1;
+
 
 // Torus - Donut
 const torusGeo = new THREE.TorusGeometry(2, 1, 30, 50);
@@ -142,10 +153,44 @@ const orbit = new OrbitControls(camera, renderer.domElement);
 orbit.update();
 
 
+// Raycaster - Get Mouse position
+const rayCaster = new THREE.Raycaster();
+const mousePosition = new THREE.Vector2();
+window.addEventListener("mousemove", (e) => {
+    mousePosition.x = (e.clientX / window.innerWidth) * 2 - 1;
+    mousePosition.y = - (e.clientY / window.innerHeight) * 2 + 1;
+})
+
+// Get objects/Meshes from ID/Name
+const torusID = torus.id;
+box2.name = "theBox";
+
 // Animations
 const animate = (time) => {
-    box.rotation.x = time/1000;
-    box.rotation.y = time/1000;
+
+    // Raycaster
+    rayCaster.setFromCamera(mousePosition, camera);
+    const intersects = rayCaster.intersectObjects(scene.children);
+    // console.log(intersects);
+
+    for (let i=0; i < intersects.length; i++) {
+        if(intersects[i].object.id === torusID) {
+            intersects[i].object.material.color.set(0xFF0000);
+        };
+        if(intersects[i].object.name === "theBox") {
+            // Box Rotation Animation
+            intersects[i].object.rotation.x = time/1000;
+            intersects[i].object.rotation.y = time/1000;
+        };
+    };
+
+    // Animate vertices values for meshes/objects
+    plane2.geometry.attributes.position.array[0] = 10 * Math.random();
+    plane2.geometry.attributes.position.array[1] = 10 * Math.random();
+    plane2.geometry.attributes.position.array[2] = 10 * Math.random();
+    plane2.geometry.attributes.position.array[lastPointZ] = 10 * Math.random();
+    plane2.geometry.attributes.position.needsUpdate = true;
+
     renderer.render(scene, camera);
 };
 
